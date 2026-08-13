1bcf207e9e76e65d8ddd10e66db49457b5013e9a
j7867 <l88858718@gmail.com>
2026-08-13 22:58:35 +0300

    script.js

--- script.js
+++ script.js
@@ -589,5 +589,5 @@ if (document.readyState === 'loading') {
 } else {
     checkUserSession();
     if (typeof loadSavedServersFromMemory === 'function') loadSavedServersFromMemory();
-}
       });
+    }
